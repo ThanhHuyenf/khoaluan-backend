dc82af33e6406daab7741c12e53398b1647a3359
Chi Dung <chidung091@gmail.com>
2021-12-06 20:41:26 +0700

    Update cors and change routes name

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ const express = require('express');
 const sequelize = require('./config/mysql')
 const routes = require('./routes/index.routes')
 const {verifyToken} = require("./middlewares/auth");
+const cors = require('cors')
 
 const app = express();
 const port = PORT;
 
+app.use(cors())
 app.use(express.json());
 sequelize.sync();
 
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,6 +2,6 @@ const userController = require('../controller/user.controller');
 const router = require('express').Router()
 const userRoutes = require('./user.routes')
 const authRoutes = require('./auth.routes')
-router.use('/user',userRoutes)
+router.use('/users',userRoutes)
 router.use('/',authRoutes)
 module.exports = router
