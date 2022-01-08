bd6a179e229a9f4264f57263b7b8fa721641bd65
Chi Dung <chidung091@gmail.com>
2022-01-08 10:06:56 +0700

    update change Password

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,12 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common'
 import {
   ApiBearerAuth,
   ApiBody,
+  ApiExcludeEndpoint,
   ApiOperation,
   ApiResponse,
   ApiTags,
 } from '@nestjs/swagger'
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard'
+import RoleGuard from '../auth/guard/role.guard'
+import { ChangePasswordDto } from './dto/change-password.dto'
 import { CreateUsersDto } from './dto/create-users.dto'
+import { Role } from './users.enum'
 import { UsersService } from './users.service'
 
 @ApiBearerAuth()
@@ -16,10 +21,26 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiExcludeEndpoint()
+  @UseGuards(RoleGuard(Role.Admin))
+  @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: 'Tạo tài khoản sinh viên mới' })
   @ApiBody({ type: CreateUsersDto })
   @ApiResponse({ status: 201, description: 'Success', type: CreateUsersDto })
   async create(@Body() dto: CreateUsersDto) {
     return await this.usersService.createUser(dto)
+  }
+
+  @Post('/change-password')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Đổi pass tài khoản' })
+  @ApiBody({ type: ChangePasswordDto })
+  @ApiResponse({ status: 201, description: 'Success', type: CreateUsersDto })
+  async changePassword(@Req() req, @Body() dto: ChangePasswordDto) {
+    return await this.usersService.changePassword(
+      req.user.userID,
+      dto.oldPassword,
+      dto.newPassword,
+    )
   }
 }
