9edb6f10502b96cd7eab008fe35dcb75bce03bb3
Chi Dung <chidung091@gmail.com>
2022-01-09 16:49:25 +0700

    fix code

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AdminService } from './admin.service'
 import { AdminController } from './admin.controller'
 import { UsersService } from '../users/users.service'
+import { UsersModule } from '../users/users.module'
 
 @Module({
-  imports: [UsersService],
+  imports: [UsersModule],
   providers: [AdminService],
   controllers: [AdminController],
 })
